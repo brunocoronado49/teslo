abda7ffa6c4a81df8151d6bb3dd390d582687b4a
brunocoronado49 <francorangelcoronado@gmail.com>
2025-02-13 14:30:55 -0800

    Update mapper in user and products to fix a bug

--- lib/features/products/presentation/screens/products_screen.dart
+++ lib/features/products/presentation/screens/products_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:teslo/features/products/presentation/providers/providers.dart';
 import 'package:teslo/features/shared/shared.dart';
 
 class ProductsScreen extends StatelessWidget {
@@ -29,8 +31,28 @@ class ProductsScreen extends StatelessWidget {
   }
 }
 
-class _ProductsView extends StatelessWidget {
+class _ProductsView extends ConsumerStatefulWidget {
   const _ProductsView();
+
+  @override
+  _ProductsViewState createState() => _ProductsViewState();
+}
+
+class _ProductsViewState extends ConsumerState<_ProductsView> {
+  final ScrollController scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    // TODO: scroll infinito
+    ref.read(productsProvider.notifier).loadNextPage();
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
